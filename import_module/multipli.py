b226814b408be73480cfbc30d9e1dacddf5eb8be
thomas Duprez <thomas.duprez10@gmail.com>
2019-10-18 15:31:24 +0200

    ajout d'un test de la fonction table

--- import_module/multipli.py
+++ import_module/multipli.py
@@ -1,5 +1,7 @@
 # -*-coding:UTF-8 *-
 
+import os
+
 def table(nb, max=10):
     """Fonction affichant la table de multiplication par nb de 1 * jusqu'a max * nb """
 
@@ -8,3 +10,9 @@ def table(nb, max=10):
     while i < max:
         print(i + 1, " * ", nb, " = ", (i + 1) * nb)
         i += 1
+
+# test de la fonction table
+
+if __name__ == "__main__":
+    table(4)
+    os.system("pause")
