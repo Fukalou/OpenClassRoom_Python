f3a0c2a583663bf5170ea66d4f8696e2c7c1c191
thomas Duprez <thomas.duprez10@gmail.com>
2019-10-18 15:17:59 +0200

    Création du dossier pour l'exercice des import et développement de la fonction table

---
+++ import_module/multipli.py
@@ -0,0 +1,10 @@
+# -*-coding:UTF-8 *-
+
+def table(nb, max=10):
+    """Fonction affichant la table de multiplication par nb de 1 * jusqu'a max * nb """
+
+    i = 0
+
+    while i < max:
+        print(i + 1 " * ", nb, " = ", (i + 1) * nb)
+        i += 1
